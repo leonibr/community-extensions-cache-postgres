6eb7764ef33a3e706addbe4bf64617f850016cfe
Ashley Marques <marques.ashley@gmail.com>
2018-03-22 17:43:06 -0300

    Removed References to sql

--- Extensions.Caching.PostgreSql/PostgreSqlCacheServiceCollectionExtensions.cs
+++ Extensions.Caching.PostgreSql/PostgreSqlCacheServiceCollectionExtensions.cs
@@ -7,40 +7,40 @@ using Microsoft.Extensions.Caching.Distributed;
 
 namespace Community.Microsoft.Extensions.Caching.PostgreSql
 {
-    /// <summary>
-    /// Extension methods for setting up PostgreSql distributed cache services in an <see cref="IServiceCollection" />.
-    /// </summary>
-    public static class PostGreSqlCachingServicesExtensions
-    {
-        /// <summary>
-        /// Adds Microsoft PostgreSql distributed caching services to the specified <see cref="IServiceCollection" />.
-        /// </summary>
-        /// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
-        /// <param name="setupAction">An <see cref="Action{SqlServerCacheOptions}"/> to configure the provided <see cref="SqlServerCacheOptions"/>.</param>
-        /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
-        public static IServiceCollection AddDistributedPostgreSqlCache(this IServiceCollection services, Action<PostgreSqlCacheOptions> setupAction)
-        {
-            if (services == null)
-            {
-                throw new ArgumentNullException(nameof(services));
-            }
+	/// <summary>
+	/// Extension methods for setting up PostgreSql distributed cache services in an <see cref="IServiceCollection" />.
+	/// </summary>
+	public static class PostGreSqlCachingServicesExtensions
+	{
+		/// <summary>
+		/// Adds Community Microsoft PostgreSql distributed caching services to the specified <see cref="IServiceCollection" />.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection" /> to add services to.</param>
+		/// <param name="setupAction">An <see cref="Action{PostgreSqlCacheOptions}"/> to configure the provided <see cref="PostgreSqlCacheOptions"/>.</param>
+		/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+		public static IServiceCollection AddDistributedPostgreSqlCache(this IServiceCollection services, Action<PostgreSqlCacheOptions> setupAction)
+		{
+			if (services == null)
+			{
+				throw new ArgumentNullException(nameof(services));
+			}
 
-            if (setupAction == null)
-            {
-                throw new ArgumentNullException(nameof(setupAction));
-            }
+			if (setupAction == null)
+			{
+				throw new ArgumentNullException(nameof(setupAction));
+			}
 
-            services.AddOptions();
-            AddPostgreSqlCacheServices(services);
-            services.Configure(setupAction);
+			services.AddOptions();
+			AddPostgreSqlCacheServices(services);
+			services.Configure(setupAction);
 
-            return services;
-        }
+			return services;
+		}
 
-        // to enable unit testing
-        internal static void AddPostgreSqlCacheServices(IServiceCollection services)
-        {
-            services.Add(ServiceDescriptor.Singleton<IDistributedCache, PostgreSqlCache>());
-        }
-    }
+		// to enable unit testing
+		internal static void AddPostgreSqlCacheServices(IServiceCollection services)
+		{
+			services.Add(ServiceDescriptor.Singleton<IDistributedCache, PostgreSqlCache>());
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# Extensions.Caching.PostgreSQL
+
+# Community.Microsoft.Extensions.Caching.PostgreSQL
+
+## Description
+
+This implemantation uses PostgreSQL as distributed cache.
+
+## Getting Started
+1. Install the package into your project
+```
+dotnet add package Communit.Microsoft.Extensions.Caching.PostgreSQL
+```
+2. Add the following line to the `Startup`  `Configure` method.
+```
+services.AddDistributedPostgreSqlCache()
+```
+
+
 Distributed cache implementation of IDistributedCache using PostgreSQL
 
 Available on NuGet at [https://www.nuget.org/packages/Extensions.Caching.PostgreSql/](https://www.nuget.org/packages/Extensions.Caching.PostgreSql/)
+
+# License
+* MIT
+### This is a fork from [repo](https://github.com/wullemsb/Extensions.Caching.PostgreSQL)
